1e763eaf6e00da890565d8a6280926178f928e9d
Devansh Shankhdhar <shankhdhardevansh29@gmail.com>
2026-03-13 21:59:40 +0530

    Fix login URL and clean up admin.js

--- admin.js
+++ admin.js
@@ -17,7 +17,7 @@ async function login(){
     const password = document.getElementById("admin-password").value;
 
     try{
-        const res = await fetch("http://127.0.0.1:8000/login",{
+        const res = await fetch("https://yandmacademy.onrender.com/",{
             method:"POST",
             headers: {"Content-Type":"application/json"},
             body: JSON.stringify({username, password})
