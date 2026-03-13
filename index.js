a8a508deec914caee11684e46d95f7a817f4e9ba
Devansh Shankhdhar <shankhdhardevansh29@gmail.com>
2026-03-13 22:05:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // FETCH EVENT
 async function loadEvent(){
-    const res = await fetch("http://127.0.0.1:8000/event");
+    const res = await fetch("https://yandmacademy.onrender.com/event");
     const data = await res.json();
     document.getElementById("eventHeading").innerText = data.heading || "";
     document.getElementById("eventDescription").innerText = data.description || "";
@@ -9,7 +9,7 @@ loadEvent();
 
 // FETCH OFFER
 async function loadOffer(){
-    const res = await fetch("http://127.0.0.1:8000/offer");
+    const res = await fetch("https://yandmacademy.onrender.com/offer");
     const data = await res.json();
     document.getElementById("offerHeading").innerText = data.heading || "";
     document.getElementById("offerDescription").innerText = data.description || "";
